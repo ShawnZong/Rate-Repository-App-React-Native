db1d1c20603ee938b99c4214d9b7565479f5dac9
shawnjstan <29206292+ShawnZong@users.noreply.github.com>
2021-03-01 11:55:20 +0200

    set token header to each request

--- frontend/App.js
+++ frontend/App.js
@@ -1,13 +1,17 @@
 // config
 import React from "react";
+import AuthStorage from "./src/utils/authStorage";
+
 // apollo
 import { ApolloProvider } from "@apollo/react-hooks";
 import createApolloClient from "./src/utils/apolloClient";
-const apolloClient = createApolloClient();
 
 // components
 import { NativeRouter } from "react-router-native";
 import Main from "./src/components/Main";
+
+const authStorage = new AuthStorage();
+const apolloClient = createApolloClient(authStorage);
 
 const App = () => {
   return (
